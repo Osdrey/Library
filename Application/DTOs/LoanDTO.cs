953e25ee69cc930ff5e0870c2e2713ee1c9e0cf6
Osdrey <oscardaniloreyesb@gmail.com>
2025-06-13 12:19:40 -0500

    CreateDTOs
    Se crean los Data Transfer Object con sus Mappers para el correcto manejo de las entidades y sus atributos, respetando la arquitectura.

---
+++ Application/DTOs/LoanDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Application.DTOs
+{
+    public class LoanDTO
+    {
+        public int LoanId { get; set; }
+        public int ReservationId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public string LoanStatus { get; set; }
+    }
+}
---
+++ Application/DTOs/UserDTO.cs
@@ -0,0 +1,21 @@
+﻿using Library.Domain.Enumerations;
+
+namespace Library.Application.DTOs
+{
+    public class UserDTO
+    {
+        public int Cedula { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
+        public int Age { get; set; }
+
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserType UserType { get; set; }
+        public UserRole UserRole { get; set; }
+        public int Arrears { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
