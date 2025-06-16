08fba5622f6213971e0529ba5580516b9c4a1a58
Osdrey <oscardaniloreyesb@gmail.com>
2025-06-16 12:36:45 -0500

    createDAOs
    Se crea la configuración de conexión a la base de datos, inicialmente con usuarios, conexión inicial para la autenticación.

---
+++ Infraestructure/Exceptions/DAOException.cs
@@ -0,0 +1,23 @@
+﻿namespace Library.Infraestructure.Exceptions
+{
+    internal class DAOException
+    {
+        public class DataAccessException : Exception
+        {
+            public DataAccessException(string message, Exception? inner = null)
+                : base(message, inner) { }
+        }
+
+        public class ConnectionException : DataAccessException
+        {
+            public ConnectionException(Exception? inner = null)
+                : base("Error al conectar con la base de datos.", inner) { }
+        }
+
+        public class ConfigurationException : DataAccessException
+        {
+            public ConfigurationException(string message, Exception? inner = null)
+                : base($"Error de configuración de base de datos: {message}", inner) { }
+        }
+    }
+}
---
+++ Infraestructure/Interfaces/IUserDAO.cs
@@ -0,0 +1,15 @@
+﻿using Library.Application.DTOs;
+
+namespace Library.Infraestructure.Interfaces
+{
+    public interface IUserDAO
+    {
+        List<UserDTO> SearchAllUsers();
+        UserDTO? SearchUser(string input);
+        void CreateUser(UserDTO user);
+        void UpdateUser(UserDTO user);
+        void ReactivateUser(int cedula);
+        void DeactivateUser(int cedula);
+        void ChangePassword(string username, string newPasswordHash);
+    }
+}
